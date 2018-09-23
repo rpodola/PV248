f5be80904b9251937d0203d3ba597fc8c50b810a
Radim Podola <rpodola@gmail.com>
2018-09-23 17:50:01 +0200

    Parsing and counting Composer stats

--- 01-text/stat.py
+++ 01-text/stat.py
@@ -1,5 +1,23 @@
 #! python3.7
 import sys
-sys.stdout.write("hello from Python %s\n" % (sys.version,))
+import re
+from collections import Counter
 
+sys.stdout.write("hello from Python %s\n" % (sys.version))
 input("Press Enter to continue...")
+
+composerStats = Counter()
+
+with open('./scorelib.txt', 'r', encoding="utf-8") as file:
+  for line in file:
+    composerLine = re.match( r'^Composer: (.*)', line)
+    if composerLine:
+      names = re.split("; ?", composerLine.group(1))
+      for name in names:
+        name = re.sub(r'[(].*[)]', '', name).strip()
+        if name:
+          composerStats[name] += 1 
+          print("name: ", name.encode(sys.stdout.encoding, errors='replace'))
+
+for k, v in composerStats.items():
+  print("{0}: {1}".format(k.encode(sys.stdout.encoding, errors='replace'), v))
