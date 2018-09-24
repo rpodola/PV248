78355e7131364997f4accf963d22e64e79038707
Radim Podola <rpodola@gmail.com>
2018-09-24 22:56:21 +0200

    Remove non-standard modules

--- 01-text/stat.py
+++ 01-text/stat.py
@@ -1,10 +1,9 @@
-#! python3.7
+#! python3
 import sys
 import re
-from pathlib import Path
 import collections
 import argparse
-from dateutil.parser import parse as dateParse
+from os import path
 
 
 PARSER_FUNC_PREFIX = 'parse_line_'
@@ -18,14 +17,15 @@ def eprint(*args, **kwargs):
   print(*args, file=sys.stderr, **kwargs)
 
 
-def parse_args(*args,**kwargs):
+def parse_args():
   "This function parses command-line arguments and does basic checks"
   parser = argparse.ArgumentParser()
   parser.add_argument("filename", help="source file")
-  parser.add_argument("mode", help="statistics mode of aggregation (supported modes: composer, century)")
+  parser.add_argument("mode", help="statistics mode of aggregation "
+                                   "(supported modes: composer, century)")
   args = parser.parse_args()
 
-  if not Path(args.filename).is_file():
+  if not path.isfile(args.filename):
     eprint("Filename doesn't refer to a valid file")
     exit(2)
   if args.mode not in (COMPOSER_MODE, CENTURY_MODE):
@@ -46,7 +46,7 @@ def parse_file(filename, mode, stats):
 
 def parse_line_composer(line, stats):
   "This function parses line and alter composer statistic data"
-  composerLine = re.match( r'^Composer: (.*)', line)
+  composerLine = re.match(r'^Composer: (.*)', line)
   if composerLine:
     names = re.split(";", composerLine.group(1))
     for name in names:
@@ -57,28 +57,25 @@ def parse_line_composer(line, stats):
 
 def parse_line_century(line, stats):
   "This function parses line and alter century statistic data"
-  def centuryFromYear(year):
+  def century_from_year(year):
     return (year - 1) // 100 + 1
 
-  centuryLine = re.match( r'^Composition Year:(.*)', line)
+  centuryLine = re.match(r'^Composition Year:(.*)', line)
   if centuryLine:
-    year = centuryLine.group(1).strip()
-    if year:
-      try:
-        year = dateParse(year).year
-      except ValueError:
-        res = re.search(r".*(\d{4}).*", year)
+    parsedYear = centuryLine.group(1).strip()
+    if parsedYear:
+      res = re.search(r".*(\d{4}).*", parsedYear)
+      if res:
+        year = res.group(1)
+      else:
+        res = re.search(r".*(\d{2}).*century", parsedYear)
         if res:
-          year = res.group(1)
+          year = res.group(1) + "00"
         else:
-          res = re.search(r".*(\d{2}).*century", year)
-          if res:
-            year = res.group(1) + "00"
-          else:
-            year = None
+          year = None
 
       if year:
-        stats[centuryFromYear(int(year))] += 1
+        stats[century_from_year(int(year))] += 1
 
 
 def print_stats(mode, stats):
@@ -89,8 +86,8 @@ def print_stats(mode, stats):
 def print_stats_composer(stats):
   "This function prints composer mode statistics"
   ordered = collections.OrderedDict(sorted(stats.items()))
-  for k, v in ordered.items():
-    print("{0}: {1}".format(k.encode(sys.stdout.encoding, errors='replace'), v))
+  for key, value in ordered.items():
+    print("{0}: {1}".format(key.encode(sys.stdout.encoding, errors='replace'), value))
 
 
 def print_stats_century(stats):
@@ -98,16 +95,15 @@ def print_stats_century(stats):
   def get_century_suffix(century):
     if century == 1:
       return "st"
-    elif century == 2:
+    if century == 2:
       return "nd"
-    elif century == 3:
+    if century == 3:
       return "rd"
-    else:
-      return "th"
+    return "th"
 
   ordered = collections.OrderedDict(sorted(stats.items()))
-  for k, v in ordered.items():
-    print("{0}{1} century: {2}".format(k, get_century_suffix(k), v))
+  for key, value in ordered.items():
+    print("{0}{1} century: {2}".format(key, get_century_suffix(key), value))
 
 
 if __name__ == "__main__":
